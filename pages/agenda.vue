06e8d190feb041f4bdd05fe3c546b7bee2f02b73
Philip <philip.gazagnes@gmail.com>
2021-11-24 19:43:37 +0100

    page agenda

---
+++ pages/agenda.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    Agenda en construction ! Ca va venir très vite, en attendant, RDV sur notre
+    page Facebook pour vous tenir au courant des prochains concerts ;)<br />
+    <a href="https://www.facebook.com/funlive34/">Facebook Fun Live</a>
+  </div>
+</template>
