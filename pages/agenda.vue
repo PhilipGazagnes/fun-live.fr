741901898fe8b039ba882d4ae4f26b6f4f4240ab
Philip <philip.gazagnes@gmail.com>
2021-11-24 19:47:22 +0100

    agenda design

--- pages/agenda.vue
+++ pages/agenda.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
+  <div
+    style="color: white; text-align: center; padding: 50px 20px; font-size: 2em"
+  >
     Agenda en construction ! Ca va venir très vite, en attendant, RDV sur notre
     page Facebook pour vous tenir au courant des prochains concerts ;)<br />
-    <a href="https://www.facebook.com/funlive34/">Facebook Fun Live</a>
+    <a
+      style="color: white; font-weight: bold"
+      href="https://www.facebook.com/funlive34/"
+      >Facebook Fun Live</a
+    >
   </div>
 </template>
