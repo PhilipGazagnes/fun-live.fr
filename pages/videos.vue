616bb136d146052544e75047c43d0b5ba13c04f3
Philip <p.gazagnes@vacanceselect.com>
2022-01-06 15:59:39 +0100

    fix

--- pages/videos.vue
+++ pages/videos.vue
@@ -127,13 +127,13 @@ export default {
 }
 .modal {
   position: fixed;
+  display: none;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  transform: translateY(100%);
   &[data-open] {
-    transform: translateY(0);
+    display: block;
   }
   & > button {
     position: absolute;
